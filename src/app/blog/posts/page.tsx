61c98bc0a55b9dea6132b9ed7a9cfaa6a7a775fa
jianswe <kuangjian89@outlook.com>
2024-10-03 01:16:53 -0500

    Add database connection

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,15 +1,20 @@
-import { posts } from '@/app/lib/placeholder-data'
+import Link from 'next/link'
+import { Button } from "@/app/ui/components/button"
 import Post from '@/app/ui/components/posts/Post'
+import { connectToDB, getPosts } from '@/app/lib/data'
 
-export default function Page() {
+export default async function Page() {
+    const client = await connectToDB()
+    const posts = await getPosts()
     return (
         <>
+            {client && <p className='text-green-500'>Connected to database</p>}
+            <Link href="/blog/post/insert"><Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button></Link>
             <h1>Posts</h1>
-            <ul>
-            {posts.map(post => (
+            {posts?.map((post) => (
+                // @ts-expect-error: Suppress type error temporarily
                 <Post key={post.id} {...post} />
             ))}
-            </ul>
         </>
         
     )
