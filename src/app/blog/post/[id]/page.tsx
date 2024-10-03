02380518115272c7ddb8d817ea94e41caea74f24
jianswe <kuangjian89@outlook.com>
2024-10-02 23:44:13 -0500

    Add tailwind plugin for font size

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -6,7 +6,7 @@ export default function Page({ params }: { params: { id: string } }) {
     return (
         <>
             <h1>Post</h1>
-            <Post {...post} />
+            {post && <Post {...post} />}
         </>
         
     )
